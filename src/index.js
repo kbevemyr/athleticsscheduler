43a1c41f5e95a56fcbda3d3d8db7c0755dbe5b37
Katrin Boberg Bevemyr <katrin@bevemyr.com>
2019-06-13 17:18:39 +0200

    App is no longer a route just a component.

--- src/index.js
+++ src/index.js
@@ -33,10 +33,12 @@ const store = createStore(
 ReactDOM.render((
   <Provider store={store}>
     <Router>
-      <Route path="/" component={App} />
+      <App />
     </Router>
   </Provider>
 ), document.getElementById('root'));
+
+// <Route path="/" component={App} />
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
