0aff4efb05ec6acd4c1880e9bfc571be2c57006e
Katrin Boberg Bevemyr <katrin@bevemyr.com>
2019-10-14 16:55:38 +0200

    removed waring and updated styling och Page

--- src/Page.js
+++ src/Page.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
+// I index.html länkas css-paketet http://tachyons.io/
 // first className: "bg-white shadow-1 center pa4"
+// shadow-1 get en ide om ytan som ska skrivas ut, tas bort när man går live
+//bg-white är background-color: white
 
 const Page = ({children, singleMode, id}) => (<div
   id={id} className="bg-white shadow-1"
